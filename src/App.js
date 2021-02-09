a645897d36138b2ae23721be9b2b597de3aa0117
Zach <hzballard2494@gmail.com>
2021-02-09 12:11:12 -0500

    mapping over list items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import TodoItem from './components/TodoItem'
 import Header from './components/Header'
+import todosData from './todoData'
 
 function App () {
+  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>);
+
   return (
       <div> 
         <Header/>
-        <TodoItem/> 
-        <TodoItem/> 
-        <TodoItem/> 
-        <TodoItem/> 
+        {todoItems};
       </div>
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function TodoItem () {
+function TodoItem (props) {
   return (
       <div className="checkbox"> 
-        <input type="checkbox" className="checkboxInput" id="checkbox1" name="checkbox1"/>
-        <label htmlFor="checkbox1"> checkbox1</label> 
+        <input type="checkbox" className="checkboxInput" id="checkbox1" name="checkbox1" checked={props.item.completed}/>
+        <label htmlFor="checkbox1"> {props.item.text} </label> 
       </div>
   )
 }
