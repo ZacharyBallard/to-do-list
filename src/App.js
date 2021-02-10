9cb709ba54624d70cf30453ab69d418cc4604faa
Zach <hzballard2494@gmail.com>
2021-02-10 10:40:08 -0500

    organizing state data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoItem from './components/TodoItem'
 import Header from './components/Header'
 import todosData from './todoData'
 
-function App () {
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>);
+class App extends Component {
+  constructor(props){
+    super(props);
 
-  return (
-      <div> 
-        <Header/>
-        {todoItems};
-      </div>
-  )
+    this.state = {
+      todos: todosData
+    }
+  }
+
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>);
+
+    return (
+        <div> 
+          <Header/>
+          {todoItems};
+        </div>
+    )
+  }
 }
 
 export default App;
