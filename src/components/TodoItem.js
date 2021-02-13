9080c8135a8d955e2b9c1cbf0197951ee791e31d
Zach <hzballard2494@gmail.com>
2021-02-13 14:35:21 -0500

    updated checked items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,15 +6,23 @@ function TodoItem (props) {
     color: "black",
     textDecoration: "line-through"
 }
-  
+  const date = new Date();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const time = `${hour}:${minutes}`
+
   return (
       <div className="checkbox"> 
+        <label htmlFor={props.item.key} style={props.item.completed ? completedStyle: null}> {props.item.completed ? `${props.item.text} ${time}` : props.item.text}
         <input 
-        type="checkbox" 
-        className="checkboxInput" 
-        onChange={() => props.handleChange(props.item.id)}
-        checked={props.item.completed} />
-        <p style={props.item.completed ? completedStyle: null}> {props.item.text} </p> 
+          id={props.item.key}
+          type="checkbox" 
+          className="checkboxInput" 
+          onChange={() => props.handleChange(props.item.id)}
+          checked={props.item.completed} 
+          style={{display: 'none'}}
+        />
+        </label>
       </div>
   )
 }
